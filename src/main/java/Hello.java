c407a14d73002cbdbe62931e1cfc408c1e1aed82
Stephanie <stephanie.galvez.c@gmail.com>
2021-01-26 15:39:32 -0500

    master branch watching on the living room on 10 min

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,4 +4,5 @@ public class Hello {
         System.out.println("Hello World B22");
         System.out.println("B21 Learning Git future SDET");
     }
+    //Ohh I like this movie! who played it? Let me continue
 }

b52be9ea9070425407a6fd695ed36516ee3ae242
Stephanie <stephanie.galvez.c@gmail.com>
2021-01-26 15:37:52 -0500

    movie2 branch watching on the phone for 10 minutes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World B22");
         System.out.println("B21 Learning Git future SDET");
+        //movie on the phone 10 min
     }
 }
